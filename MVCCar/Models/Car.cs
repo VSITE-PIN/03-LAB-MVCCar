663e1697f4aa11407ea428a1ff809b0f385e18f6
ivan0301 <ivangrgic0301@gmail.com>
2024-11-26 18:42:50 +0100

    Rjesen labos radi sve uredno.

---
+++ MVCCar/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace MVCCar.Models
+{
+    public class Car
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int ProductionYear { get; set; }
+        public decimal Price { get; set; }
+        public string Manufacturer { get; set; }
+
+    }
+}
