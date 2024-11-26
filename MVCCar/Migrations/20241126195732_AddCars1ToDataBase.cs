8a3d34ab941a21483bb635d952fbb2d81e52f474
Lovre Kešina <lokesina@vsite.hr>
2024-11-26 21:00:05 +0100

    lab_3

---
+++ MVCCar/Migrations/20241126195732_AddCars1ToDataBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCCar.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCars1ToDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Car",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Car");
+        }
+    }
+}
