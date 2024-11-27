5da69fe3352f5a6076f0a98b615ef7c47ea059bb
breski-igor <igor.breski@vsite.hr>
2024-11-27 04:42:15 +0100

    igor_breski_lab03

---
+++ MVCCar/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace MVCCar.Models
+{
+    public class Car
+    {
+        public int id { get; set; }
+        public String name { get; set; }
+        public String Color {  get; set; } 
+        public int ProductionYear {  get; set; }
+        public decimal Price {  get; set; }
+        public string Manufacturer { get; set; }
+
+    }
+}
