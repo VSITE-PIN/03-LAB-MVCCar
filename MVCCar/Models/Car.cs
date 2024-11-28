2be957035253c35feca34d29ed76df140cfef6ba
Juled Halili <juhalili@vsite.hr>
2024-11-28 19:33:17 +0100

    Rijeseno do 5. koraka (CarsController dodan zadnji)

---
+++ MVCCar/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace MVCCar.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public int ProductionYear { get; set; } = 0;
+        public decimal Price { get; set; }
+    }
+}
+
+
+
