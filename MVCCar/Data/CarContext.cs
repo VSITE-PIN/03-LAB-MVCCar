b8e3f18033aadd4bbef2d1e8ea161a4f47ae21ef
Bruno Barbarić <brbarbar@vsite.hr>
2024-11-28 18:41:08 +0100

    Vjezba 3.

---
+++ MVCCar/Data/CarContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCCar.Models;
+
+namespace MVCCar.Data
+{
+    public class CarContext : DbContext
+    {
+        public CarContext(DbContextOptions<CarContext> options) : base(options) { }
+
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Car>()
+                .Property(c => c.Price)
+                .HasColumnType("decimal(18,2)");  // Postavlja preciznost i skalu za Price
+        }
+    }
+}
