0d87ada3d84d062b51e38875edebbc3be39b907b
Juled Halili <juhalili@vsite.hr>
2024-11-28 19:49:36 +0100

    Dodan Manufacturer u klasu Car te azuriranje baze

--- MVCCar/Migrations/20241128184716_AddManufacturer.Designer.cs
+++ MVCCar/Migrations/20241128184716_AddManufacturer.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVCCar.Migrations
 {
     [DbContext(typeof(MVCLibraryContext))]
-    partial class MVCLibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20241128184716_AddManufacturer")]
+    partial class AddManufacturer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MVCCar/Models/Car.cs
+++ MVCCar/Models/Car.cs
@@ -7,6 +7,8 @@
         public string Color { get; set; } = string.Empty;
         public int ProductionYear { get; set; } = 0;
         public decimal Price { get; set; }
+
+        public string Manufacturer { get; set; }
     }
 }
 
