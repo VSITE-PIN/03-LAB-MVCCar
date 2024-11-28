9e0e8813ae03c35504631087a39457860d7230e4
Kristijan Popović <krpopovi@vsite.hr>
2024-11-28 19:56:12 +0100

    Gotovo

---
+++ MVCCar/Migrations/20241128183419_Cars.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCCar.Migrations
+{
+    /// <inheritdoc />
+    public partial class Cars : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductionYear = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
---
+++ MVCCar/Models/Cars.cs
@@ -0,0 +1,12 @@
+﻿namespace MVCCar.Models
+{
+    public class Cars
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int ProductionYear { get; set; }
+        public decimal Price { get; set; }
+        public string Manufracturer { get; set; }
+    }
+}
